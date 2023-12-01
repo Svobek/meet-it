9978a622e31c6ec15bc3e98197123c62b0e115aa
aJpaRmp <138137627+aJpaRmp@users.noreply.github.com>
2023-12-01 22:27:47 +0100

    Add files via upload

--- MEETIT/wwwroot/log.js
+++ MEETIT/wwwroot/log.js
@@ -7,47 +7,49 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function loginUser() {
     var apiUrl = 'https://meeeetit.azurewebsites.net/User/Login/';
-    // Get values from input fields
-    var loginValue = document.getElementById('loginUser').value;
-    var passwordValue = document.getElementById('loginPassword').value;
+// Get values from input fields
+var loginValue = document.getElementById('loginUser').value;
+var passwordValue = document.getElementById('loginPassword').value;
 
-    // Validate input values (you may want to add more validation)
-    if (!loginValue || !passwordValue) {
-        console.error('Login and password are required');
-        return;
+// Validate input values (you may want to add more validation)
+if (!loginValue || !passwordValue) {
+    console.error('Login and password are required');
+    return;
+}
+
+var user = {
+    login: loginValue,
+    psswd: passwordValue
+};
+
+fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(user)
+})
+.then(response => {
+    console.log('Response:', response);
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
     }
-
-    var user = {
-        login: loginValue,
-        psswd: passwordValue
-    };
-
-    fetch(apiUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(user)
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        console.log('Status: ', response.status);
-        return response.json();
-    })
-    .then(data => {
-        if (data.success) {
-            console.log('Login successful:', data);
-            // Redirect to the desired page after successful login
-            window.location.href = 'index.html';
-        } else {
-            console.error('Login failed. Incorrect username or password. Try again.');
-            // Display an error message on the page
-            document.getElementById('loginErrorMessage').textContent = 'Incorrect username or password. Try again.';
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+    console.log('Status: ', response.status);
+    return response.json();
+})
+.then(data => {
+    console.log('Data:', data);
+    if (data.success) {
+        console.log('Login successful:', data);
+        // Redirect to the desired page after successful login
+        window.location.href = 'index.html';
+    } else {
+        console.error('Login failed. Incorrect username or password. Try again.');
+        // Display an error message on the page
+        document.getElementById('loginErrorMessage').textContent = 'Incorrect username or password. Try again.';
+    }
+})
+.catch(error => {
+    console.error('Fetch error:', error);
+});
 }
