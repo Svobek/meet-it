4206db005cc22ac87230c00f54cab0d3538439a1
Svobek <135534769+Svobek@users.noreply.github.com>
2023-12-05 18:58:02 +0100

    Update log.js

--- MEETIT/wwwroot/log.js
+++ MEETIT/wwwroot/log.js
@@ -1,54 +1,3 @@
-/*document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('loginButton').addEventListener('click', function () {
-        loginUser();
-    });
-});
-
-function loginUser() {
-    let apiUrl = 'https://meeetit.azurewebsites.net/User/Login/';
-    let loginValue = document.getElementById('loginUser').value;
-    let passwordValue = document.getElementById('loginPassword').value;
-
-    if (!loginValue || !passwordValue) {
-        console.error('Login and password are required');
-        return;
-    }
-
-    let user = {
-        login: loginValue,
-        psswd: passwordValue
-    };
-
-    fetch(apiUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(user)
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        console.log("321");
-        console.log(response);
-        return response.text();
-    })
-    .then(data => {
-        console.log(data);
-        console.log("123");
-        if (data === "Succesfull Login") {
-            alert("Zalogowano pomyślnie!");
-            window.location.href = '/index.html';
-        } else {
-            document.getElementById('loginErrorMessage').textContent = 'Incorrect username or password. Try again.';
-            alert('Login failed. Incorrect username or password. Try again.');
-        }
-    })
-    .catch(error => {
-        console.error('Fetch error:', error);
-    });
-}*/
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('loginButton').addEventListener('click', function () {
         event.preventDefault();
@@ -79,25 +28,24 @@ async function loginUser() {
             },
             body: JSON.stringify(user)
         });
-
+        let data = await response.text();
         if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
+            if (data === "Invalid login or password"){
+                document.getElementById('loginErrorMessage').textContent = 'Incorrect username or password. Try again.';
+            }
         }
 
         console.log("321");
         console.log(response);
 
-        let data = await response.text();
+        
         console.log(data);
         console.log("123");
 
         if (data === "Succesfull Login") {
             alert("Zalogowano pomyślnie!");
             window.location.href = '/index.html';
-        } else {
-            document.getElementById('loginErrorMessage').textContent = 'Incorrect username or password. Try again.';
-            alert('Login failed. Incorrect username or password. Try again.');
-        }
+        } 
     } catch (error) {
         console.error('Fetch error:', error);
     }
